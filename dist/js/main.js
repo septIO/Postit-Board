b0105cb2528d4992baaf5dda9a235421251f02e4
septIO <in@sept.io>
2016-05-26 15:22:38 +0200

    .
    .

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 const HSB = "hsb",
       RGB = "rgb";
 
-var postits = [];
+postits = [];
 
 /**
  * How should colors be used?
@@ -15,67 +15,38 @@ Run();
 /**
  * Initialize the app and draw the default / saved postits
  */
-function Run(){
+function Run() {
     postits.push(new Postit());
     for (var i = 0; i < postits.length; i++) {
         postits[i].draw();
     }
-    createEvenetListeners();
 }
 
-function createEvenetListeners(){
-    var notes = document.getElementsByClassName("postit-handle");
-    
-    for (var i = 0; i < notes; i++) {
-        notes[i].addEventListener('click', test, false);
+function Postit() {
+    this.text = '';
+    this.pos = new Vector(10, 10);
+    this.background = new Color(Random(128), 75, 85, 100);
+    this.isActive = false;
+    this.id = 
+
+    this.draw = function () {
+        // Create main element
+        $el = $('<div class="postit-container"></div>');
+        $el.css({
+            "background-color": this.background.toCSS(),
+            "left": this.pos.x + "%",
+            "top": this.pos.y + "%"
+        });
+        $el.append('<div class="postit-handle"></div>');
+
+        // Create body element
+        $body = $('<div class="postit-body"></div>');
+        $body.append('<textarea placeholder="Enter your text here">' + this.text + '</textarea>');
+
+        // Create main element in the DOM
+        $el.append($body);
+        $('body').append($el);
     }
-}
-
-var test = function(){
-    alert('test');
-}
-
-function Postit(){
-    this.text='';
-    this.pos = new Vector(10,10);
-    this.zindex = 0;
-    this.backgroundColor = new Color(Random(100), 100, 75);
-    this.HTML;
-    this.isActive = true;
-
-    this.moveTo = function(newPosition){
-        this.pos = newPosition; // Do some animation shit here;
-    }
-
-    this.draw = function(){
-        if(typeof this.HTML === 'undefined')
-            this.HTML = new PostitHTMLObject(this);
-        
-        document.body.appendChild(this.HTML);
-    }
-}
-
-function PostitHTMLObject(postitObject){
-    var container = document.createElement("div");
-    container.className = "postit-container";
-
-    if(postitObject.isActive){
-        container.className += " active";
-    }
-
-    container.id = "p"+postits.length;
-    container.style.cssText = "z-index:" + postitObject.zindex + ";top:" + postitObject.pos.y + "%;left:" + postitObject.pos.x + "%;background-color:" + postitObject.backgroundColor.toCSS() + ";";
-
-    var handle = document.createElement("div");
-    handle.className = "postit-handle";
-    container.appendChild(handle);
-
-    var body = document.createElement("div");
-    body.className = "postit-body";
-    body.innerHTML = "<textarea placeholder='Enter your text here'>"+postitObject.text+"</textarea>";
-    container.appendChild(body);
-
-    return container;
 }
 
 /**
@@ -83,7 +54,7 @@ function PostitHTMLObject(postitObject){
  * @param {integer} x x-coordinate
  * @param {integer} y y-coordinate
  */
-function Vector(x,y){
+function Vector(x, y) {
     this.x = x;
     this.y = y;
 }
@@ -95,47 +66,46 @@ function Vector(x,y){
  * @param {integer} b     Blue channel value (0-255)
  * @param {integer} a=100 Alpha channel value (0-100)
  */
-function Color(r,g,b,alpha=100){
+function Color(r, g, b, alpha) {
     this.r = r;
     this.g = g;
     this.b = b;
-    this.alpha = alpha;
+    this.alpha = alpha || 100
 
-    this.toCSS = function(){
+    this.toCSS = function () {
         switch (colorMethod) {
             case HSB:
 
-                return "hsla("+r+", "+g+"%,"+b+"%, "+alpha+")";
+                return "hsla(" + r + ", " + g + "%," + b + "%, " + alpha + ")";
                 break;
             case RGB:
-                return "rgba("+r+","+g+","+b+","+alpha+")";
+                return "rgba(" + r + "," + g + "," + b + "," + alpha + ")";
                 break;
         }
 
     }
 }
 
+/**
+ * Generate a random number between min (inclusive) and max (exclusive)
+ * second parameter can be omitted to generate a random value between 0 and X
+ * @param {integer} min min value, or max value if second parameter is not used
+ * @param {integer} max max value
+ */
+function Random(min, max) {
+    var a = min,
+        b = max;
+
     /**
-     * Generate a random number between min (inclusive) and max (exclusive)
-     * second parameter can be omitted to generate a random value between 0 and X
-     * @param {integer} min min value, or max value if second parameter is not used
-     * @param {integer} max max value
+     * Check if max value has been set, if it hasn't
+     * then set min value to 0, and max value to the parsed integer
+     * @param  {integer} typeof max           === 'undefined' Check if max was parsed
+     * @return {void}
      */
-    function Random(min,max){
-        var a = min,
-            b = max;
-
-        /**
-         * Check if max value has been set, if it hasn't
-         * then set min value to 0, and max value to the parsed integer
-         * @param  {integer} typeof max           === 'undefined' Check if max was parsed
-         * @return {void}
-         */
-        if(typeof max === 'undefined'){
-            a = 0,
-            b = min;
-        }
-
-        return ~~(Math.random() * (b - a + 1)) + a;
+    if (typeof max === 'undefined') {
+        a = 0,
+        b = min;
     }
-    
+
+    return ~~(Math.random() * (b - a + 1)) + a;
+}
